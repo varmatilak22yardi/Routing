774e12fb270f61d0171e1707509ebfff3d346aac
Tilak Varma <tv35302@YSIDEV.YARDI.CLOUD>
2025-02-04 14:57:37 +0530

    adding about route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet,RouterModule } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { TodolistComponent } from "./todolist/todolist.component";
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [RouterOutlet,RouterModule, HomeComponent, TodolistComponent],
+  imports: [RouterOutlet,RouterModule, HomeComponent, TodolistComponent,AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TodolistComponent } from './todolist/todolist.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'todolist', component: TodolistComponent },
+  {path:'about',component:AboutComponent},
   { path: '**', redirectTo: '' } // Redirect unknown routes to home
 ];
