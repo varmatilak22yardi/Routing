341c63d1d9415edfd14bcdcd2777e3265baf14ec
Tilak Varma <tv35302@YSIDEV.YARDI.CLOUD>
2025-02-04 13:59:46 +0530

    Adding HashLocationStartegy in main.ts file

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-
+import {LocationStrategy,HashLocationStrategy} from '@angular/common';
 
 bootstrapApplication(AppComponent,{
-  providers:[provideRouter(routes)]
+  providers:[provideRouter(routes),
+    {provide:LocationStrategy,
+      useClass:HashLocationStrategy
+    }
+  ]
 })
   .catch((err) => console.error(err));
