447c99227041142cd9ece5b55079343a4d8641a8
Tilak Varma <tv35302@YSIDEV.YARDI.CLOUD>
2025-02-04 14:37:23 +0530

    changes in route

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app/app.routes';
 import {LocationStrategy,HashLocationStrategy} from '@angular/common';
 
 bootstrapApplication(AppComponent,{
-  providers:[provideRouter(routes),
+  providers:[provideRouter(routes,withHashLocation()),
     {provide:LocationStrategy,
       useClass:HashLocationStrategy
     }
